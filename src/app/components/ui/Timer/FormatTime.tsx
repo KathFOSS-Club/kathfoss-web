90d7a451aedda1fba508ddf54fc7cbfd8a86c1a6
nischalshakya <107358076+nischalshakya787@users.noreply.github.com>
2025-01-17 09:41:59 +0545

    fix: added padStart which ensure the digits are always two digits

--- src/app/components/ui/Timer/FormatTime.tsx
+++ src/app/components/ui/Timer/FormatTime.tsx
@@ -6,10 +6,11 @@ interface FormatTimeProps {
   unit: string;
 }
 export default function FormatTime({ value, unit }: FormatTimeProps) {
+  const formattedValue = String(value).padStart(2, "0"); // Ensures two digits
   return (
     <>
       <Typography variant="h2" sx={{ fontSize: "6vw" }}>
-        {value}
+        {formattedValue}
         <Typography variant="body1" sx={{ fontSize: "2vh" }}>
           {value === 1 ? unit : `${unit}s`}
         </Typography>
