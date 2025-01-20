49b769f84d3f8e47716853a9c41d116751c9ce86
nischalshakya <107358076+nischalshakya787@users.noreply.github.com>
2025-01-20 20:28:20 +0545

    HeroSection

--- src/app/components/ui/Timer/FormatTime.tsx
+++ src/app/components/ui/Timer/FormatTime.tsx
@@ -4,21 +4,31 @@ import React from "react";
 export default function FormatTime({ value }: { value: number }) {
   const formattedValue = String(value).padStart(2, "0"); // Ensures two digits
   return (
-    <>
-      <Typography
-        variant="h1"
-        sx={{
-          textAlign: "center",
-          lineHeight: "4rem",
-          backgroundColor: "secondary.main",
-          fontFamily: "DS-Digital",
-          width: "6rem",
-          paddingBottom: "12px",
-          borderRadius: 3,
-        }}
-      >
-        {formattedValue}
-      </Typography>
-    </>
+    <Typography
+      sx={{
+        fontSize: {
+          xs: "2rem", // Smaller font size for extra-small screens
+          sm: "3.6rem", // Default font size for small screens and above
+        },
+        textAlign: "center",
+        lineHeight: {
+          xs: "2.5rem", // Adjusted line height for smaller screens
+          sm: "4rem", // Default line height for larger screens
+        },
+        backgroundColor: "secondary.main",
+        fontFamily: "DS-Digital, sans-serif",
+        width: {
+          xs: "4rem", // Smaller width for extra-small screens
+          sm: "6rem", // Default width for larger screens
+        },
+        paddingBottom: {
+          xs: "8px", // Smaller padding for smaller screens
+          sm: "12px", // Default padding for larger screens
+        },
+        borderRadius: 3,
+      }}
+    >
+      {formattedValue}
+    </Typography>
   );
 }
