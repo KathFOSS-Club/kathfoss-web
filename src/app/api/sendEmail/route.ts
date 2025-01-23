fe36a1099b2aedfcd28e041514724314cfbb87ee
Your Name <your-email@example.com>
2025-01-23 09:46:32 +0545

    contact with required

--- src/app/api/sendEmail/route.ts
+++ src/app/api/sendEmail/route.ts
@@ -1,28 +1,29 @@
+
 import { Resend } from 'resend';
 import { z } from 'zod';
 import { contactSchema } from '@/utils/contactSchema';
+
 const resend = new Resend(process.env.RESEND_API_KEY!);
 
 export async function POST(req: Request) {
   try {
     const body = await req.json();
 
- 
-
+    const formData = contactSchema.parse(body);
     // Construct the email content
     const emailContent = `
-      <p><strong>Name:</strong> ${body.name}</p>
-      <p><strong>Phone:</strong> ${body.phone}</p>
-      <p><strong>Email:</strong> ${body.email}</p>
+      <p><strong>Name:</strong> ${formData.name}</p>
+      <p><strong>Phone:</strong> ${formData.phone}</p>
+      <p><strong>Email:</strong> ${formData.email}</p>
       <p><strong>Message:</strong></p>
-      <p>${body.message}</p>
+      <p>${formData.message}</p>
     `;
 
     // Send the email
     const result = await resend.emails.send({
       from: 'onboarding@resend.dev',
       to: 'kathfoss@kathford.edu.np',
-      subject: `New Contact Form Submission from ${body.name}`,
+      subject: `New Contact Form Submission from ${formData.name}`,
       html: emailContent,
     });
 
